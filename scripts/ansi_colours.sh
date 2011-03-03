b37d12f33f536ec6b5f2f9109b5f264a6695e4ca
Pádraig Brady <P@draigBrady.com>
2011-03-03 09:47:20 +0000

    ansi_colours.sh: add row and col headers
    Suggested at
    http://comments.pixelbeat.org/docs/terminal_colours/#comment-185521

--- scripts/ansi_colours.sh
+++ scripts/ansi_colours.sh
@@ -7,15 +7,26 @@
 # Changes:
 #    V0.1, 24 Apr 2008, Initial release
 #    V0.2, 30 Oct 2009, Support dash
+#    V0.3, 03 Mar 2011
+#      http://github.com/pixelb/scripts/commits/master/scripts/ansi_colours.sh
 
 e="\033["
+vline=$(tput smacs; printf 'x'; tput rmacs)
+
+#printf "${e}4m%80s${e}0m\n"
+printf "${e}1;4mf\\\\b${e}0m${e}4m none  white    black     red     \
+green    yellow   blue    magenta    cyan  ${e}0m\\n"
+
+rows='brgybmcw'
+
 for f in 0 7 `seq 6`; do
-  no="" bo=""
+  no=""; bo=""; p=""
   for b in n 7 0 `seq 6`; do
-    co="3$f"; p="  "
-    [ $b = n ] || { co="$co;4$b";p=""; }
-    no="${no}${e}${co}m   ${p}${co} ${e}0m"
-    bo="${bo}${e}1;${co}m ${p}1;${co} ${e}0m"
+    co="3$f"; [ $b = n ] || co="$co;4$b"
+    no="${no}${e}${co}m  ${p}${co} ${e}0m"
+    bo="${bo}${e}1;${co}m${p}1;${co} ${e}0m"
+    p=" "
   done
-  printf "$no\n$bo\n"
+  fc=$(echo $rows | cut -c$((f+1)))
+  printf "$fc $vline$no\nb$fc$vline$bo\n"
 done
